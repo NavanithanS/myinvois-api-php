b82bb07e086fc2b76cdcfbf1aeb8337d4f3378aa
Nava <gua@navins.biz>
2024-12-19 08:42:52 +0800

    Fix ApiException constructor to accept string message

--- src/Exception/ApiException.php
+++ src/Exception/ApiException.php
@@ -2,18 +2,12 @@
 
 namespace Nava\MyInvois\Exception;
 
-class ApiException extends \Exception
+use Exception;
+
+class ApiException extends Exception
 {
-    protected array $context;
-
-    public function __construct(string $message, array $errors = [], int $code = 422, ?\Throwable $previous = null)
+    public function __construct(string $message, int $code = 0, Exception $previous = null)
     {
-        $this->errors = $errors;
-        parent::__construct($message, ['errors' => $errors], $code, $previous);
-    }
-
-    public function getContext(): array
-    {
-        return $this->context;
+        parent::__construct($message, $code, $previous);
     }
 }
