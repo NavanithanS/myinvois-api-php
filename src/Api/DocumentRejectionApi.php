5b4c22b9a845c81f09c59bf7673abca2128c3b78
Lee Guang You <guangyou1386@gmail.com>
2025-01-13 17:09:57 +0800

    modify

--- src/Api/DocumentRejectionApi.php
+++ src/Api/DocumentRejectionApi.php
@@ -11,7 +11,7 @@ use Psr\Log\LoggerInterface;
  */
 trait DocumentRejectionApi
 {
-    protected ?LoggerInterface $logger = null;
+    protected $logger = null;
 
     /**
      * Reject a document.
